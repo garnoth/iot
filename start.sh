b79679d3f9c490c0bf5580079807411e93040d59
Peter V <garnoth@uw.edu>
2021-02-28 07:13:08 -0800

    fix start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,11 @@
 #/bin/bash
 certdir=/home/pi/cert/
+prefix=cb11952a43
 endpoint=a1w6lfyecyp33x-ats.iot.us-west-2.amazonaws.com
 rootCA=AmazonRootCA3.pem
-cert=cb11952a43-certificate.pem.crt
-key=cb11952a43-private.pem.key
+cert=$prefix-certificate.pem.crt
+key=$prefix-private.pem.key
+name=RPI-GW-0
 
-python3 mainLoop.py --endpoint $endpoint --root-ca $certdir$rootCA --cert $certdir$cert --key $certdir$key --topic "sensors/+/gateway" --client-id "RPI-GW-01"
+python3 mainLoop.py --endpoint $endpoint --root-ca $certdir$rootCA --cert $certdir$cert --key $certdir$key --topic "sensors/+/gateway" --client-id $name
 #python3 pubsub.py --endpoint $endpoint --root-ca $certdir$rootCA --cert $certdir$cert --key $certdir$key --topic "sensor/+/gateway" --client-id RPI-GW-01
